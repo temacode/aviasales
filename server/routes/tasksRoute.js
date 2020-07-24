52756e1ebc857d1623af5e1abf848962bb1d0c1d
Артём Михайлов <artem@MacBook-Pro-Artem-2.local>
2020-07-25 01:08:18 +0300

    Some lint fixes

--- server/routes/tasksRoute.js
+++ server/routes/tasksRoute.js
@@ -1,45 +1,32 @@
 module.exports = (app, pool) => {
-    app.get(`/api/shared`, (req, res) => {
-        const query = `INSERT INTO shared_users (shared, email) VALUES ('false', '') RETURNING id;`;
+    app.get('/api/shared', (req, res) => {
+        const query = 'INSERT INTO shared_users (shared, email) VALUES (\'false\', \'\') RETURNING id;';
 
         pool.query(query)
-        .then(queryResult => {
-            let userId = queryResult.rows[0].id;
+            .then(queryResult => {
+                let userId = queryResult.rows[0].id;
 
-            res.status(200).send({id: userId});
-        })
-        .catch(e => {
-            console.error(e.stack);
+                res.status(200).send({ id: userId });
+            })
+            .catch(e => {
+                console.error(e.stack);
 
-            res.status(500).sent('Server error');
-        });
+                res.status(500).sent('Server error');
+            });
     });
-    app.post(`/api/shared`, (req, res) => {
-        const query = `UPDATE shared_users SET shared=$1, email=$2 WHERE id=$3 RETURNING id, shared, email`;
+    app.post('/api/shared', (req, res) => {
+        const query = 'UPDATE shared_users SET shared=$1, email=$2 WHERE id=$3 RETURNING id, shared, email';
 
         const userInfo = req.body;
 
-        pool.query(query, [userInfo.shared, userInfo.email, userInfo.id])
-        .then(queryResult => {
-            res.status(200).send(queryResult);
-        })
-        .catch(e => {
-            console.error(e.stack);
+        pool.query(query, [ userInfo.shared, userInfo.email, userInfo.id ])
+            .then(queryResult => {
+                res.status(200).send(queryResult);
+            })
+            .catch(e => {
+                console.error(e.stack);
 
-            res.status(500).sent('Server error');
-        });
+                res.status(500).sent('Server error');
+            });
     });
-    app.delete(`/api/shared`, (req, res) => {
-        const query = `TRUNCATE shared_users`;
-
-        pool.query(query)
-        .then(queryResult => {
-            res.status(200).send('Все пользователи успешно удалены');
-        })
-        .catch(e => {
-            console.error(e.stack);
-
-            res.status(500).sent('Server error');
-        });
-    });
-}
+};
--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const colors = require("./helpers/term-colors");
+const colors = require('./helpers/term-colors');
 const { Pool } = require('pg');
 
 const connectionString = 'postgresql://artem:6070050@localhost:5432/aviasales';
@@ -9,28 +9,6 @@ const pool = new Pool({
     connectionString: connectionString,
 });
 pool.connect();
-
-// Запрос на создание базы данных
-
-/* const query = `CREATE TABLE shared_users (
-    id SERIAL PRIMARY KEY,
-    shared Boolean NOT NULL DEFAULT FALSE,
-    email varchar(255)
-)`; */
-
-//Запрос на добавление в таблицу с возвратом поля id
-//const query = `INSERT INTO shared_users (shared, email) VALUES ('TRUE', 'lol@test.ru') RETURNING id;`;
-
-//Возвращает пользователя с id
-//const query = `SELECT * FROM shared_users WHERE id=2`;
-
-//Обновление данных о пользователе с id
-//const query = `UPDATE shared_users SET shared=true, email='artem@icloud.com' WHERE id=2`;
-
-/* pool.query(query, (err, res) => {
-    console.log(err, res);
-    pool.end();
-}); */
 
 const app = express();
 const port = process.env.PORT || 5000;
